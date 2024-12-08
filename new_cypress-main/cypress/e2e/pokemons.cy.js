59724908f02bb375449a779d070a2c09266ddc7e
vlad-tarandyuk-qa <vlad.tarandyuk@gmail.com>
2024-12-08 12:20:46 +0500

    Update pokemons.cy.js

--- new_cypress-main/cypress/e2e/pokemons.cy.js
+++ new_cypress-main/cypress/e2e/pokemons.cy.js
@@ -1,8 +1,8 @@
 describe('Покупка аватара', function () {
     it('e2e тест на покупку нового аватара для тренера', function () {
          cy.visit('https://pokemonbattle.ru/'); // переходим на сайт
-         cy.get('input[type="email"]').type('vlad-tarandyuk1987@yandex.ru'); // вводим логин
-         cy.get('input[type="password"]').type('Tve101187'); // вводим пароль
+         cy.get('input[type="email"]').type('USER_LOGIN'); // вводим логин
+         cy.get('input[type="password"]').type('USER_PASSWORD'); // вводим пароль
          cy.get('button[type="submit"]').click(); // нажимаем кнопку Подтвердить
          cy.wait(2000);
          cy.get('.header__container > .header__id').click({ force: true }); // Клик на аву тренера
